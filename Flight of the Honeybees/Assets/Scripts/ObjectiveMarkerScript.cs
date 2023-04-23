25133f3a61e3d79dfe83cdc6f13633929ed28df8
WillSwift01 <103675097+WillSwift01@users.noreply.github.com>
2023-04-23 01:38:23 -0400

    Detailed Level and Finish In-Game Journal
    Gate is finished, Trees have been meshed, Journal in scene works, but triggers have not been added in yet. Created playscene border to prevent out of bounds falling. Fixed issue with waypoint code.

--- Flight of the Honeybees/Assets/Scripts/ObjectiveMarkerScript.cs
+++ Flight of the Honeybees/Assets/Scripts/ObjectiveMarkerScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ObjectiveMarkerScript : MonoBehaviour
 {
-
+    public Camera playerCamera;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +15,6 @@ public class ObjectiveMarkerScript : MonoBehaviour
     void LateUpdate()
     {
         //Forced the UI of the Canvas w/ this script to constantly face the camera
-        transform.LookAt(transform.position + Camera.current.transform.rotation * Vector3.forward, Camera.current.transform.rotation * Vector3.up);
+        transform.LookAt(transform.position + playerCamera.transform.rotation * Vector3.forward, playerCamera.transform.rotation * Vector3.up);
     }
 }
