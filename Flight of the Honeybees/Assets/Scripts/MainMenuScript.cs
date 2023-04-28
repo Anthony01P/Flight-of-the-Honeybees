1860a0cd60130e2745141d81f633ed3faa648f2b
WillSwift01 <103675097+WillSwift01@users.noreply.github.com>
2023-04-28 10:44:55 -0400

    UI interaction code for Main Menu
    in the name

--- Flight of the Honeybees/Assets/Scripts/MainMenuScript.cs
+++ Flight of the Honeybees/Assets/Scripts/MainMenuScript.cs
@@ -6,22 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
-    public GameObject optionsMenu;
-    public GameObject mainMenu;
     public void StartButton()
     {
         SceneManager.LoadScene(1);
     }
-    public void OptionsButton()
-    {
-        optionsMenu.SetActive(true);
-        mainMenu.SetActive(false);
-    }
-    public void BackButton()
-    {
-        optionsMenu.SetActive(false);
-        mainMenu.SetActive(true);
-    }
+    
     public void QuitButton()
     {
         Debug.Log("The Game has been Quit.");
