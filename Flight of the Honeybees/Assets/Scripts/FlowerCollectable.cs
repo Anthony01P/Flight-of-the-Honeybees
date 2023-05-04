075fff7ce85dbde5936529d5c871769700f65c80
WillSwift01 <103675097+WillSwift01@users.noreply.github.com>
2023-05-04 04:59:51 -0400

    Adding in Stuff Again
    Sounds and their proper placing, UI implementation done, Tutorial has text, NEW Assets Scene for team to view assets in editor.

--- Flight of the Honeybees/Assets/Scripts/FlowerCollectable.cs
+++ Flight of the Honeybees/Assets/Scripts/FlowerCollectable.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class FlowerCollectable : MonoBehaviour
 {
     public GameObject arrow;
+    public AudioSource collectedPollen;
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Player")
         {
+            collectedPollen.Play();
             arrow.SetActive(false);
             this.gameObject.tag = "Collected";
         }
